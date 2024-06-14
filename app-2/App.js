853f7bdcd8ca0d38d0f9b2f7402a5316851dae96
JuAnMoRa49 <125929793+JuAnMoRa49@users.noreply.github.com>
2024-06-14 15:24:49 -0600

    Se implementaron estilos
    Se checo la creación de estilos y como llamarlos, ademas de flexbox y orientación de los componentes

--- app-2/App.js
+++ app-2/App.js
@@ -4,8 +4,9 @@ import { StyleSheet, Text, View, Button } from 'react-native';
 import { useState } from 'react';
 
 // Componente Texto que recibe contenido como prop y lo muestra
-const Texto = ({ contenido }) => {
-  return <Text style={styles.text}>{contenido}</Text>;
+const Texto = ({ contenido, estilo }) => {
+  // Se aplica el estilo del componente text
+  return <Text style={[styles.text, estilo]}>{contenido}</Text>;
 };
 
 export default function App() {
@@ -21,9 +22,16 @@ export default function App() {
 
   return (
     <View style={styles.container}>
+
+      {/* Se cran los textos con contenido con diferentes bg */}
+      <Texto estilo={styles.bgRed} contenido={contenido}/>
+      <Texto estilo={styles.bgGreen} contenido={contenido}/>
+      <Texto estilo={styles.bgBlue} contenido={contenido2}/>
+
+
       {/* Se manda llamar la propiedad contenido en el componente Texto */}
-      <Texto contenido={contenido} />
-      <Texto contenido={contenido2} />
+      {/* <Texto contenido={contenido} />
+      <Texto contenido={contenido2} /> */}
 
       {/* Botón que al presionarlo actualiza el estado */}
       <Button title='Press' color="#000000" onPress={actualizarContenido} />
@@ -36,8 +44,37 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    // Se puede modificar la dirección del flex, column de arriba hacia abajo, row de izquierda a derecha
+    flexDirection:'column',
+    // Permite modificar la posición de los elementos en horizontal si se trabaja con flex column
+    alignItems:'center',
+    justifyContent: 'space-between',
     backgroundColor: '#fff',
-    alignItems: 'center',
     justifyContent: 'center',
   },
+  // Se establece el estilo que se le quiere dar a text
+  text: {
+    color:'white',
+    fontSize:'2rem',
+    margin:'1rem',
+    backgroundColor:'black',
+    height:100,
+    width:200,
+  },
+  // Se crean varios fondos para despues usarlos en los textos
+  bgRed:{
+    backgroundColor: 'red',
+    // Flex con valor de 1/6
+    // flex:1,
+  },
+  bgGreen:{
+    backgroundColor: 'green',
+    // Flex con valor de 2/6
+    // flex:2,
+  },
+  bgBlue:{
+    backgroundColor: 'blue',
+    // Flex con valor de 3/6
+    // flex:3,
+  }
 });
